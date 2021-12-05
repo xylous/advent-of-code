e54a0f637fdf7857888cdb6fe898dce4805c1313
xylous <xylous.e@gmail.com>
2021-12-05 09:05:24 +0200

    Add solutions for 2021 day 5 part 2

--- 2021/day5.hs
+++ 2021/day5.hs
@@ -12,8 +12,10 @@ main :: IO ()
 main = do
     input <- fmap lines (readFile "input5.txt")
     let clouds = map parseCloud input
-    let points = map allPointsInHorizontalCloud clouds
-    print (length (getDups (concat points)))
+    let horizontalLines = map (allPointsInCloud False) clouds
+    let withDiagonals = map (allPointsInCloud True) clouds
+    print (length (getDups (concat horizontalLines)))
+    print (length (getDups (concat withDiagonals)))
 
 getDups :: Ord a => [a] -> [a]
 getDups = map head . filter (\l -> length l > 1) . group . sort
@@ -36,18 +38,26 @@ parseCloud s = Cloud (parsePoint a) (parsePoint b)
     b = last points
     points = words s
 
-allPointsInHorizontalCloud :: Cloud -> [Point]
-allPointsInHorizontalCloud (Cloud (Point x1 y1) (Point x2 y2))
-    | x1 == x2 && y1 > y2 = a : allPointsInHorizontalCloud y1Smaller
-    | x1 == x2 && y2 > y1 = b : allPointsInHorizontalCloud y2Smaller
-    | y1 == y2 && x1 > x2 = a : allPointsInHorizontalCloud x1Smaller
-    | y1 == y2 && x2 > x1 = b : allPointsInHorizontalCloud x2Smaller
+allPointsInCloud :: Bool -> Cloud -> [Point]
+allPointsInCloud matchDiagonals (Cloud (Point x1 y1) (Point x2 y2))
+    | x1 == x2 && y1 > y2 = a : allPointsInCloud matchDiagonals straightY1
+    | x1 == x2 && y2 > y1 = b : allPointsInCloud matchDiagonals straightY2
+    | y1 == y2 && x1 > x2 = a : allPointsInCloud matchDiagonals straightX1
+    | y1 == y2 && x2 > x1 = b : allPointsInCloud matchDiagonals straightX2
+    | matchDiagonals && x1 > x2 && y1 > y2 = b : allPointsInCloud matchDiagonals diagX1Y1
+    | matchDiagonals && x2 > x1 && y2 > y1 = a : allPointsInCloud matchDiagonals diagX2Y2
+    | matchDiagonals && x2 > x1 && y1 > y2 = a : allPointsInCloud matchDiagonals diagX2Y1
+    | matchDiagonals && x1 > x2 && y2 > y1 = a : allPointsInCloud matchDiagonals diagX1Y2
     | x1 == x2 && y1 == y2 = [a]
     | otherwise = []
       where
         a = Point x1 y1
         b = Point x2 y2
-        y1Smaller = Cloud (Point x1 (y1 - 1)) (Point x2 y2)
-        y2Smaller = Cloud (Point x1 y1) (Point x2 (y2 - 1))
-        x1Smaller = Cloud (Point (x1 - 1) y1) (Point x2 y2)
-        x2Smaller = Cloud (Point x1 y1) (Point (x2 - 1) y2)
+        straightY1 = Cloud (Point x1 (y1 - 1)) (Point x2 y2)
+        straightY2 = Cloud (Point x1 y1) (Point x2 (y2 - 1))
+        straightX1 = Cloud (Point (x1 - 1) y1) (Point x2 y2)
+        straightX2 = Cloud (Point x1 y1) (Point (x2 - 1) y2)
+        diagX1Y1 = Cloud (Point x1 y1) (Point (x2 + 1) (y2 + 1))
+        diagX2Y2 = Cloud (Point (x1 + 1) (y1 + 1)) (Point x2 y2)
+        diagX2Y1 = Cloud (Point (x1 + 1) (y1 - 1)) (Point x2 y2)
+        diagX1Y2 = Cloud (Point (x1 - 1) (y1 + 1)) (Point x2 y2)
