11191d4bf9619f147f117ac9f55f56c24cf05470
xylous <xylous.e@gmail.com>
2022-01-19 18:20:13 +0200

    Add solution for 2015 day 3, part 2

--- 2015/day03.hs
+++ 2015/day03.hs
@@ -8,7 +8,18 @@ data Pos = Pos
 main :: IO ()
 main = do
     input <- head . lines <$> readFile "input03.txt"
+    -- first part
     print $ length . nub . snd $ mapAccumL (\x y -> (nextPos x y,x)) (Pos 0 0) input
+    -- second part: we only need to change the input!
+    let santa = snd $ mapAccumL (\x y -> (nextPos x y,nextPos x y)) (Pos 0 0) $ evens input
+    let robosanta = snd $ mapAccumL (\x y -> (nextPos x y,nextPos x y)) (Pos 0 0) $ odds input
+    print $ length . nub $ santa ++ robosanta ++ [Pos 0 0]
+
+odds :: [a] -> [a]
+odds xs = [xs !! n | n <- [1,3..length xs-1]]
+
+evens :: [a] -> [a]
+evens xs = [xs !! n | n <- [0,2..length xs-1]]
 
 nextPos :: Pos -> Char -> Pos
 nextPos (Pos x y) c = case c of '>' -> Pos (x+1) y
